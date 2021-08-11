3610aa6fb5aead8acc55764859f3cdf15ad57ff5
Stephen Giang <stepheng753@gmail.com>
2021-08-11 16:36:45 -0700

    added line v rect mode

--- Tutor/JavaScript/p5.js/Bubble Sort/BubbleSort.js
+++ Tutor/JavaScript/p5.js/Bubble Sort/BubbleSort.js
@@ -1,6 +1,7 @@
 let lineHeight = [];
 let lineWidth = 0;
 let numLines = 0;
+let i = 0;
 
 function setup() {
 	createCanvas(1000, 400);
@@ -9,18 +10,30 @@ function setup() {
 		lineHeight.push(int(random(0, height)));
 	}
 	lineWidth = width / numLines;
-	console.log(lineHeight);
 }
 
 function draw() {
 	colorMode(RGB);
 	background(100, 200, 300);
 
+	swap(lineHeight, Math.floor(random() * numLines), Math.floor(random() * numLines));
+
 	for (let i = 0; i < lineHeight.length; i++) {
 		colorMode(HSB);
 		let lineColor = map(lineHeight[i], 0, height, 0, 360);
-		stroke(0);
-		fill(lineColor, 100, 100);
-		rect(i * lineWidth, height - lineHeight[i], lineWidth, lineHeight[i]);
+		if (lineWidth > 1) {
+			stroke(0);
+			fill(lineColor, 100, 100);
+			rect(i * lineWidth, height - lineHeight[i], lineWidth, lineHeight[i]);
+		} else {
+			stroke(lineColor, 100, 100);
+			line(i, height, i, height - lineHeight[i]);
+		}
 	}
+}
+
+function swap(array, index1, index2) {
+	let temp = array[index1];
+	array[index1] = array[index2];
+	array[index2] = temp;
 }
