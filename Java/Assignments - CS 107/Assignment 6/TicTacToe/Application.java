82be07ae840e1c11a23d85760cb6ca79de2daf64
kuriouskadence <lykadence07@gmail.com>
2022-03-19 01:35:35 -0700

    finished assignment 6, Tic Tac Toe.

--- Java/Assignments - CS 107/Assignment 6/TicTacToe/Application.java
+++ Java/Assignments - CS 107/Assignment 6/TicTacToe/Application.java
@@ -1,11 +1,36 @@
 public class Application {
+    public static void printArr(int[] array) {
+        System.out.print("[ ");
+        for(int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.print("]");
+    }
+
+    public static void print2DArr(char[][] array) {
+        System.out.println("[ ");
+        for(int row = 0; row < array.length; row++) {
+            for(int col = 0; col < array[col].length - 1; col++){
+                System.out.print(array[row][col] + ", ");
+            }
+            System.out.println(array[row][array[row].length - 1]);
+        }
+        System.out.print("]");
+        
+    }
     public static void main(String[] args) {
-        char[][] a = {{'X','X','X'}, {'X','X','X'}, {'X', 'X', 'O'}};
         TicTacToe game = new TicTacToe();
-        game.setA(a);
-        System.out.println(a);
-        game.print2DArr(a);
-        System.out.println();
-        System.out.println(game.checkRow(2, true));
+        // game.setBoard(new char[][] {{'X', 'X', '*'}, {'*', '*', '*'}, {'*','*','*'}});
+        // System.out.println(game);
+        // game.print2DArr(game);
+        // System.out.println(game.toString());
+        // System.out.println(game);
+        // System.out.println(game.checkRow(0, true));
+        // System.out.println(game.checkRow(1, true));
+        // System.out.println(game.checkCol(1, true));
+        // System.out.println(game.checkDiag(2, true));
+        game.playGame();
+
+
     }
 }
--- Java/Assignments - CS 107/Assignment 6/TicTacToe/TicTacToe.java
+++ Java/Assignments - CS 107/Assignment 6/TicTacToe/TicTacToe.java
@@ -1,18 +1,24 @@
+import java.util.Scanner;
 public class TicTacToe {
-    int n;
     char[][] board;
-
-    public TicTacToe(int n, char[][] board){
-        this.n = n;
-        this.board = board;
-    }
+    char player1 = 'X';
+    char player2 = 'O';
 
     public TicTacToe(){
         int n = 3;
         this.board = new char[n][n];
+        for(int row = 0; row < board.length; row++) {
+            for(int col = 0; col < board[row].length; col++) {
+                board[row][col] = '*';
+            }
+        }
     }
 
-    public void setA(char[][] array){
+    public TicTacToe(char[][] board){
+        this.board = board;
+    }
+
+    public void setBoard(char[][] array){
         this.board = array;
     }
 
@@ -20,46 +26,133 @@ public class TicTacToe {
         return board;
     }
 
-    public boolean checkRow(int n, boolean isPlayerX) {
-        char player = 'X';
-        if(player != 'X') {
-            isPlayerX = false;
+    public void playGame() {
+        Scanner input = new Scanner(System.in);
+        System.out.println(printInstructions());
+        for(int i = 0; i < board.length; i++) {
+        System.out.println(toString());
+        // if(input != board.length) {
+        //     System.out.println("Try again!");
+        // }
+        System.out.print("Player 1 - enter in your row: ");
+        int row = input.nextInt();
+        System.out.print("Player 1 - enter in your column: ");
+        int col = input.nextInt();
+        if(board[row][col] == player1 || board[row][col] == player2 ) {
+            System.out.print("Player 1 - enter in your row: ");
+            row = input.nextInt();
+            System.out.print("Player 1 - enter in your column: ");
+            col = input.nextInt();
+        } else {
+            board[row][col] = player1;
         }
-        return isPlayerX;
 
-        for(int i = 0;)
-    }
-
-
-
-
-
-    // public String toString(){
-    //     String returnStr = "";
-    //     returnStr += ""
-    //     return returnStr;
-    // }
-
-    public void printArr(int[] array) {
-        System.out.print("[ ");
-        for(int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + ", ");
+        System.out.println(toString());
+        if(checkRow(row, true) == true|| checkCol(col, true) == true || checkDiag(true) == true) {
+            System.out.print("Winner!");
+        } 
+        System.out.print("Player 2 - enter in your row: ");
+        int row2 = input.nextInt();
+        System.out.print("Player 2 - enter in your column: ");
+        int col2 = input.nextInt();
+        if(board[row2][col2] == player1 || board[row2][col2] == player2 ) {
+            System.out.print("Player 2 - enter in your row: ");
+            row2 = input.nextInt();
+            System.out.print("Player 2 - enter in your column: ");
+            col2 = input.nextInt();
+        } else {
+            board[row2][col2] = player2;
         }
-        System.out.print("]");
-    }
-
-    public void print2DArr(char[][] array) {
-        System.out.print("[ ");
-        for(char col = 0; col < array.length; col++) {
-            for(char row = 0; row < array[col].length - 1; row++){
-                System.out.print(array[col][row] + ", ");
-            }
-            System.out.println(array[col][array[col].length - 1]);
+        System.out.println(toString());
+        if(checkRow(row2, true) == true|| checkCol(col2, true) == true || checkDiag(true) == true) {
+            System.out.println("Winner!");
+        } 
         }
-        System.out.print("]");
         
     }
+    
+    public String printInstructions(){
+        String str = "";
+        str += ("Welcome to TIC TAC TOE!" + "\n");
+        str += ("To play you need to have your character make in a straight line, 3 in a row." + "\n");
+        str += ("You'll start off by entering a row and column in order to decide where your player goes." + "\n");
+        str += ("From top to bottom the row numbers are: 0, 1, 2." + "\n");
+        str += ("From left to right the column numbers are: 0, 1, 2." + "\n");
+        str += ("Enjoy!!" + "\n");
+        return str;
+    }
 
+    public boolean checkRow(int n, boolean isPlayerX) {
+        char player = '\0';
+        if(isPlayerX == true) {
+            player = 'X';
+        } else{
+            player = 'O';
+        }
+        char[] row = board[n];   
 
+        for(int i = 0; i < row.length; i++) {
+            if(row[i] != player){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkCol(int n, boolean isPlayerX) {
+        char player = '\0';
+        if(isPlayerX == true) {
+            player = 'X';
+        } else {
+            player = 'O';
+        }
+        for(int i = 0; i < board.length; i++) {
+            if(board[i][n] != player) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkDiag(boolean isPlayerX) {
+        char player = '\0';
+        if(isPlayerX == true) {
+            player = 'X';
+        } else {
+            player = 'O';
+        }
+        boolean diag1 = true;
+        boolean diag2 = true;
+        int j = board.length -1;
+        for(int i = 0; i < board.length; i++) {
+            if(board[i][j--] != player) {
+                diag1 = false;
+            } if(board[i][i] != player) {
+                diag2 = false;
+            } 
+        }
+
+        if(diag1 == true) {
+            return diag1;
+        } else {
+            return diag2;
+        }
+         
+  }
+
+    @Override
+    public String toString(){
+        String returnStr = "";
+        // returnStr += "[\n";
+        for(int row = 0; row < board.length; row++) {
+            for(int col = 0; col < board[col].length - 1; col++){
+                returnStr += board[row][col] + ", ";
+            }
+            returnStr += board[row][board[row].length - 1] + "\n";
+        }
+        // returnStr += "]";
+
+        return returnStr;
+    }
     
 }
