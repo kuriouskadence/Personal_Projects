0b68268354b159002884ce576462be6791de6e52
kuriouskadence <lykadence07@gmail.com>
2022-03-23 10:00:44 -0700

    Started making methods for each class

---
+++ Java/Assignment/IntArrayList.java
@@ -0,0 +1,66 @@
+public class IntArrayList{
+    int[] arrayList;
+
+    public IntArrayList() {
+     int[] arrayList = new int[0];
+    }
+    
+    public IntArrayList(int[] array) {
+        this.arrayList = array;
+    }
+
+    public void oneAdd(int n) {
+
+    }
+
+    public void twoAdd(int n, int index) {
+
+    }
+
+    public int get(int n) {
+
+    } 
+
+    public int[] remove(int n) {
+
+    }
+
+    public int[] clear() {
+
+    }
+
+    public boolean contains(int n) {
+
+    }
+
+    public int search(int n) {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public void set(int n, int index) {
+
+    }
+
+    public int getSize() {
+
+    }
+
+    public int getSum(int n) {
+
+    }
+
+    public int getProduct(int n) {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
+
+}
