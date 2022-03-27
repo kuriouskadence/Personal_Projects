5a277e397aa0d9b5c811644555c8e6c065dc9673
kuriouskadence <lykadence07@gmail.com>
2022-03-27 03:25:02 -0700

    finished the subclasses and their methods, finished the whole code and assignment.

--- Java/Assignment/Queue.java
+++ Java/Assignment/Queue.java
@@ -6,10 +6,15 @@ public class Queue extends IntArrayList{
     }
 
     public int peek() {
-
+        return array[0];
     }
 
-    public int poll() {
-
+    public int[] poll() {
+    int[] copyArray = new int[array.length - 1];
+        for(int i = 1; i < array.length; i++) {
+            copyArray[i - 1] = array[i];
+        }
+        array = copyArray;
+        return array;
     }
 }
--- Java/Assignment/Stack.java
+++ Java/Assignment/Stack.java
@@ -6,10 +6,32 @@ public class Stack extends IntArrayList{
     }
 
     public int peek() {
-
+        return array[array.length];
     }
 
-    public int push() {
+    public int pop() {
+        int top = peek();
+        int[] copyArray = new int[array.length - 1];
+        for(int i = 0; i < array.length; i++) {
+            if(i != top) {
+                copyArray[i] = array[i];
+            }
+        }
+        array = copyArray;
+        return top;
+    }
+
+    public int[] push(int n) {
+        int[] copyArray = new int[array.length + 1];
+        for(int i = 0; i < array.length; i++) {
+            copyArray[i] = array[i + 1];
+        }
+
+        copyArray[array.length] = n;
+        
+        array = copyArray;
+        return array;
+
 
     }
 
