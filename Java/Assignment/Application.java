5b0d5b8b10f3fa99e992e9bf171dfcdd7c9413ee
kuriouskadence <lykadence07@gmail.com>
2022-03-27 03:01:04 -0700

    started working on the functions, almost done with the arraylist class, working on the recursion methods

--- Java/Assignment/Application.java
+++ Java/Assignment/Application.java
@@ -1,6 +1,8 @@
 public class Application {
     
     public static void main(String[] args) {
-        
+        int [] a = {1, 2, 3, 4};
+        IntArrayList newArrayList = new IntArrayList(a);
+        System.out.println(newArrayList);
     }
 }
--- Java/Assignment/IntArrayList.java
+++ Java/Assignment/IntArrayList.java
@@ -9,57 +9,101 @@ public class IntArrayList{
         this.arrayList = array;
     }
 
-    public void oneAdd(int n) {
-
+    public int[] add(int n) {
+        int lastIndex = arrayList.length;
+        int[] copyArray = new int[arrayList.length + 1];
+        for(int i = 0; i < arrayList.length; i++) {
+            copyArray[i] = arrayList[i];
+        }
+        copyArray[lastIndex] = n;
+        arrayList = copyArray;
+        return arrayList;
     }
 
-    public void twoAdd(int n, int index) {
+    public int[] add(int n, int index) {
+        int[] copyArray = new int[arrayList.length];
+        for(int i = 0; i < arrayList.length; i++) {
+            copyArray[i] = arrayList[i];
+        }
+        copyArray[index] = n;
 
+        return copyArray;
     }
 
     public int get(int n) {
-
+        return arrayList[n];
     } 
 
     public int[] remove(int n) {
+        int[] copyArray = new int[arrayList.length - 1];
+        for(int i = 0; i < arrayList.length; i++) {
+            if( arrayList[i] != n) {
+                copyArray[i] = arrayList[i];
+            }
+        }
 
+        return copyArray;
     }
 
     public int[] clear() {
-
+        int[] copyArray = new int[0];
+        arrayList = copyArray;
+        return arrayList;
     }
 
     public boolean contains(int n) {
-
+        for(int i = 0; i < arrayList.length; i++) {
+            if(arrayList[i] == n) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public int search(int n) {
-
+        for(int i = 0; i < arrayList.length; i++) {
+            if(arrayList[i] == n) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public boolean isEmpty() {
-
+        if(arrayList.length == 0) {
+            return true;
+        }
+        return false;
     }
+    // need some help with set because its similar to add with two perams
+    // i remember you saying something about how they were different but i cant remember
+    // public int[] set(int n, int index) {
 
-    public void set(int n, int index) {
-
-    }
+    // }
 
     public int getSize() {
-
+        return arrayList.length;
     }
 
-    public int getSum(int n) {
+    // public int getSum(int n) {
+        
+    // }
 
-    }
+    // public int getProduct(int n) {
 
-    public int getProduct(int n) {
-
-    }
+    // }
 
     @Override
     public String toString() {
+        String rtnStr = "";
+        rtnStr += "[ ";
+        for(int i = 0; i < arrayList.length; i++) {
+                rtnStr += arrayList[i] + ", " ;
+            }
+        rtnStr += "]";
 
+        return rtnStr;
+    
     }
 
 
